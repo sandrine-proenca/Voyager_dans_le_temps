4e05039c4bb39568ce88b17d986077b71e0cf876
sandrine-proenca <sandrine.proenca@sncf.fr>
2023-03-17 13:59:25 +0100

    fix create && update dto.ts

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,44 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+export class CreateUserDto {
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+    
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    password_confirm: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    firstname: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    lastname: string;
+
+    @ApiProperty()
+    @IsString()
+    birthday: string;
+
+    @ApiProperty()
+    @IsString()
+    phone: string;
+
+    @ApiProperty()
+    @IsString()
+    address: string;
+
+}
+
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,41 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    firstname: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    lastname: string;
+
+    @ApiProperty()
+    @IsString()
+    birthday: string;
+
+    @ApiProperty()
+    @IsString()
+    phone: string;
+
+    @ApiProperty()
+    @IsString()
+    address: string;
+
+}
+
