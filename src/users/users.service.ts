d1eeb3845517a2c74e1fb9fc230ee9ee508f17ba
sandrine-proenca <sandrine.proenca@sncf.fr>
2023-03-17 14:28:24 +0100

    fix create in UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async create(createUserDto: CreateUserDto) {
+    const user = await User.create({ ...createUserDto}).save();
+    delete user.password;
+    return user;
   }
 
   findAll() {
