3028ebffc196e29cdb00a097069228b1f174bc4a
sandrine-proenca <sandrine.proenca@sncf.fr>
2023-03-18 11:58:58 +0100

    fix: jwt.strategy.ts

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { UsersService } from 'src/users/users.service';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor( private readonly userService: UsersService )
+    {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken() /* The method by which the JWT will be extracted from the 'Request' */,
+            ignoreExpiration: false,
+            secretOrKey: process.env.SECRET,
+        });
+    }
+
+    async validate ( payload: any ) {
+
+        const user = await this.userService.findOne( payload.sub) /* role */
+
+        /* Condition that does not authorize if it's not the user */
+        if ( !user ){
+            throw new UnauthorizedException ()
+        }
+
+        return user
+    }
+}
