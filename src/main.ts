52f65b921638f179320f8ba5040d412d270065d3
sandrine-proenca <sandrine.proenca@sncf.fr>
2023-03-31 10:38:22 +0200

    change name database

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {  //fonction qui démarre notre appli
   const app = await NestFactory.create(AppModule, { cors: true });
   const config = new DocumentBuilder()
-    .setTitle(`album_photo`)
+    .setTitle('voyager_dans_le_temps')
     .setDescription(`API album photo`)
     .setVersion('1.0')
     .build();
